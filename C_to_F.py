e031b698d498b0fcc24d193c00e88f52a1ecbbb6
Alex Su <isoturtle1026@gmail.com>
2020-06-12 10:40:34 +0800

    Add new file

---
+++ C_to_F.py
@@ -0,0 +1,13 @@
+#x = 5      <== easy to ready
+#print('hi', x)  <== easy to ready
+#height = 180  <==
+# my_name = 'alex'
+# 1name   no good  ==> name1
+# 1 tab == 4 space
+# ctrl + a (select all)
+c = input('Please input F degree: ')
+c = float(c)
+f = c * 9 / 5 +32
+print('F degree is:', f)
+
+
